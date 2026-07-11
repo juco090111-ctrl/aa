aa2f45adf7d9993b67c52a06bf8c4a7e5252bc3e
juco090111-ctrl <juco090111@gmail.com>
2026-07-11 15:12:27 +0900

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ text1 = st.text_area("소감", placeholder = "소감입니다")
 
 if st.button("확인"):
     st.success(f"{user_id}/{grade}학년/{cla}반/{level}")
-    st.markdown(f"**질문 내용:** {score})
+    st.markdown(f"**질문 내용:** {score}")
     st.info(f"소감:{text1})
 
 
